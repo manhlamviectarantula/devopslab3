7384561673cbebc7863933e4e99658f073f727dc
Nguyen <manhlamviectarantula@gmail.com>
2024-12-23 17:25:51 +0700

    feat: update api

--- main.go
+++ main.go
@@ -13,5 +13,10 @@ func main() {
 			"message": "pong",
 		})
 	})
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
 	r.Run(":3000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
